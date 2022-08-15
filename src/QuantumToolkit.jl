a919b059617b2b91f19a5a7607b06a64e2b02c50
Jorge Gidi <jorgegidi@gmail.com>
2022-08-14 22:43:25 -0400

    move matrix density states to density_states.jl

--- src/QuantumToolkit.jl
+++ src/QuantumToolkit.jl
@@ -7,6 +7,7 @@ include("include/misc_tools.jl")
 include("include/bases.jl")
 include("include/operators.jl")
 include("include/ket_states.jl")
+include("include/density_states.jl")
 include("include/distances.jl")
 include("include/measurements.jl")
 
