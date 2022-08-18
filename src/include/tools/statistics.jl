ac6c194a41284762135b19e7f0bdae5744d35a52
Jorge Gidi <jorgegidi@gmail.com>
2022-08-18 18:30:40 -0400

    add docs

--- src/include/tools/statistics.jl
+++ src/include/tools/statistics.jl
@@ -1,5 +1,12 @@
 export simulate_experiment
 
+"""
+    simulate_experiment(value::Number, Nshots=Inf)
+
+Returns a random sample from a
+[`binomial distribution`](https://en.wikipedia.org/wiki/Binomial_distribution)
+with success probability `value` and number of trials `Nshots`.
+"""
 function simulate_experiment(value::Number, Nshots=Inf)
 
     if isinf(Nshots)
@@ -12,7 +19,14 @@ function simulate_experiment(value::Number, Nshots=Inf)
     return simulated
 end
 
-function simulate_experiment(values::Vector, Nshots=Inf)
+"""
+    simulate_experiment(value::AbstractVector, Nshots=Inf)
+
+Returns a random sample from a
+[`multinomial distribution`](https://en.wikipedia.org/wiki/Multinomial_distribution)
+with success probabilities `values` and number of trials `Nshots`.
+"""
+function simulate_experiment(values::AbstractVector, Nshots=Inf)
 
     if isinf(Nshots)
         simulated = values
