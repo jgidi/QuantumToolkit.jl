587e98bdbc6947066da2c508416870dbd50134e8
Jorge Gidi <jorgegidi@gmail.com>
2022-08-31 12:40:28 -0400

    specialize expectation_values for Hermitian matrices

--- src/include/measurements.jl
+++ src/include/measurements.jl
@@ -3,3 +3,8 @@ export expectation_values
 function expectation_values(ρ::AbstractMatrix, base::AbstractMatrix)
     return sum(conj(base) .* (ρ * base), dims=1)[:]
 end
+
+# Hermitian matrices should yield real expectation values
+function expectation_values(ρ::Hermitian, base::AbstractMatrix)
+    return sum(real, conj(base) .* (ρ * base), dims=1)[:]
+end
